369cb57ea50431ee7e3e17b2668444228a70ba83
Victoria Andressa Faria <victoriaamdf@gmail.com>
2018-10-20 13:19:32 -0300

    create class Professor

--- PracticePoo/src/practicepoo/People.java
+++ PracticePoo/src/practicepoo/People.java
@@ -3,15 +3,57 @@ package practicepoo;
 
 
 
-public class People {
+public abstract class People {
     
     protected String Name;
     protected int RG;
+    protected int Kids;
 
-    public People(String Name, int RG) {
+    public People(String Name, int RG, int Kids) {
         this.Name = Name;
         this.RG = RG;
+        this.Kids = Kids;
     }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public int getRG() {
+        return RG;
+    }
+
+    public void setRG(int RG) {
+        this.RG = RG;
+    }
+
+    public int getKids() {
+        return Kids;
+    }
+
+    public void setKids(int Kids) {
+        this.Kids = Kids;
+    }
+
+    
+    public abstract double CalcularSalario ();
+    
+    public abstract void Zerar();
+    
+    public void Hollerit(){
+        
+        System.out.println("Name: " + Name + "\n" +
+                            "RG: " + RG + "\n" +
+                            "Kids: " + Kids);
+    
+    }
+    
+    
+    
     
     
     
---
+++ PracticePoo/src/practicepoo/Professor.java
@@ -0,0 +1,54 @@
+package practicepoo;
+
+public class Professor extends People {
+
+    protected String Course;
+    protected double ClassHour;
+    protected int QttClass;
+
+    public Professor(String Name, int RG, int Kids) {
+        super(Name, RG, Kids);
+    }
+
+    public Professor(String Name, int RG, int Kids, String Course, int ClassHour) {
+        super(Name, RG, Kids);
+        this.Course = Course;
+        this.ClassHour = ClassHour;
+    }
+
+    public String getCourse() {
+        return Course;
+    }
+
+    public void setCourse(String Course) {
+        this.Course = Course;
+    }
+
+    @Override
+    public double CalcularSalario() {
+        double SalarioMensal;
+        SalarioMensal = QttClass * ClassHour;
+        return SalarioMensal;
+    }
+
+    @Override
+    public void Zerar() {
+        QttClass = 0;
+    }
+
+    @Override
+    public void Hollerit() {
+          super.Hollerit(); 
+         System.out.println("ClassHour: " + ClassHour + "\n"
+                            + "QttClass: " + QttClass + "\n"
+                            + "Salario: " + CalcularSalario());
+    }
+   
+    
+    public void RegistrarAula(int qtd) {
+        QttClass = qtd;
+    }
+    
+    
+
+} //end class Professor
